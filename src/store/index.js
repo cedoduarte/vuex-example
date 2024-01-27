d16a23801069c08249e00dd71f2c7479c5fcea63
Carlos Duarte <carlosduarte.1@hotmail.com>
2024-01-26 19:58:50 -0700

    first

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,29 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    msg: "Vuex desde 0.",
+    nombre: "Carlos",
+    apellido: "Duarte",
+    amigos: [],
+    amigo: null
   },
   getters: {
+    mensaje(state) {
+      return state.msg;
+    },
+    nombreCompleto(state) {
+      return `${state.nombre} ${state.apellido}`;
+    }
   },
   mutations: {
+    addAmigo(state) {
+      state.amigos = [...state.amigos, state.amigo];
+    }
   },
   actions: {
+    addAmigoAction(context) {
+      context.commit("addAmigo");
+    }
   },
   modules: {
   }
